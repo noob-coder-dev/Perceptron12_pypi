a3cf38ab5aa5e6963a258d3cccb724cac44bdd41
Manojit Roy <66888595+noob-coder-dev@users.noreply.github.com>
2021-10-14 18:56:55 +0530

    Shortened Requirements

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ USER_NAME = "noob-coder-dev"
 
 setuptools.setup(
     name=f"{PROJECT_NAME}-{USER_NAME}",
-    version="0.0.3",
+    version="0.0.4",
     author="Manojit Roy",
     author_email="roy.monojit982810@gmail.com",
     description="It's a real-life implementation of McCulloch-Pitts Neuron Model",
